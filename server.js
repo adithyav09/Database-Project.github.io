1bd01e60805af01f8b6bdb9a340c2395409f1333
Adithya Venkatesh <adithyav211@gmail.com>
2021-03-20 12:39:49 -0400

    Styling and 404

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
+const path = require('path');
+
 const express = require('express');
 
 const app = express();
+
+const bodyParser = require('body-parser');
 
 //Routes Admin and Shop
 const adminRoutes = require('./routes/admin.js');
 const shopRoutes = require('./routes/shop.js');
 
-const bodyParser = require('body-parser');
-
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
